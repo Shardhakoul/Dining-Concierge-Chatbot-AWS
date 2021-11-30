09524a516350dc28abb801eebbda277306d86fac
Shardhakoul <85691194+Shardhakoul@users.noreply.github.com>
2021-11-30 15:55:13 -0500

    Add files via upload

---
+++ FrontEnd/assets/js/chat.js
@@ -0,0 +1,95 @@
+var checkout = {};
+
+$(document).ready(function() {
+  var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+  $(window).load(function() {
+    $messages.mCustomScrollbar();
+    insertResponseMessage('Hi there, I\'m your personal Concierge. How can I help?');
+  });
+
+  function updateScrollbar() {
+    $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+      scrollInertia: 10,
+      timeout: 0
+    });
+  }
+
+  function setDate() {
+    d = new Date()
+    if (m != d.getMinutes()) {
+      m = d.getMinutes();
+      $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    }
+  }
+
+  function callChatbotApi(message) {
+    // params, body, additionalParams
+    return sdk.chatbotPost({}, {
+      messages: [{
+        type: 'unstructured',
+        unstructured: {
+          text: message
+        }
+      }]
+    }, {});
+  }
+
+  function insertMessage() {
+    msg = $('.message-input').val();
+    if ($.trim(msg) == '') {
+      return false;
+    }
+    $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    $('.message-input').val(null);
+    updateScrollbar();
+
+    callChatbotApi(msg)
+      .then((response) => {
+        console.log(response);
+        var data = response.data.body;
+        console.log(data, "response.data.body")
+
+        if (data.message && data.message.length > 0) {
+
+          var message = data.message;
+          console.log(message, "message")
+          insertResponseMessage(message);
+        } else {
+          insertResponseMessage('Oops, something went wrong. Please try again.');
+        }
+      })
+      .catch((error) => {
+        console.log('an error occurred', error);
+        insertResponseMessage('Oops, something went wrong. Please try again.');
+      });
+  }
+
+  $('.message-submit').click(function() {
+    insertMessage();
+  });
+
+  $(window).on('keydown', function(e) {
+    if (e.which == 13) {
+      insertMessage();
+      return false;
+    }
+  })
+
+  function insertResponseMessage(content) {
+    $('<div class="message loading new"><figure class="avatar"><img src="https://media.tenor.com/images/4c347ea7198af12fd0a66790515f958f/tenor.gif" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+    updateScrollbar();
+
+    setTimeout(function() {
+      $('.message.loading').remove();
+      $('<div class="message new"><figure class="avatar"><img src="https://media.tenor.com/images/4c347ea7198af12fd0a66790515f958f/tenor.gif" /></figure>' + content + '</div>').appendTo($('.mCSB_container')).addClass('new');
+      setDate();
+      updateScrollbar();
+      i++;
+    }, 500);
+  }
+
+});
